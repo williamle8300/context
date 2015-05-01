6cb68a8281d8e8a4c96cc61f08e817fccb10b427
williamle8300 <williamle8300@gmail.com>
2015-05-01 01:16:40 -0700

    stuff

--- browser.js
+++ browser.js
@@ -1,8 +1,7 @@
-// var matrixFun = require('./lib/matrix-fun');
-//
-// console.dir(matrixFun.data);
-// console.dir(matrixFun.offset);
-// console.dir(matrixFun.order);
-// console.dir(matrixFun.shape);
-// console.dir(matrixFun.size);
-// console.dir(matrixFun.stride);
+var fs = require('fs');
+var imdata = fs.readFileSync(__dirname + '/image.png', 'base64');
+var img = document.createElement('img');
+
+img.setAttribute('src', 'data:image/png;base64,' + imdata);
+document.body.appendChild(img);
+
